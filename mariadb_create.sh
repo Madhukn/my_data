9d3fe30ca79888916a4e0ea815e5c2d5968d0804
madhucmsclt@gmail.com <92839682+Madhukn@users.noreply.github.com>
2021-12-21 11:02:45 -0800

    Update mariadb_create.sh

--- mariadb_create.sh
+++ mariadb_create.sh
@@ -1,5 +1,5 @@
  mysql
-MariaDB > CREATE DATABASE ecomdb;
-MariaDB > CREATE USER 'ecomuser'@'localhost' IDENTIFIED BY 'ecompassword';
-MariaDB > GRANT ALL PRIVILEGES ON *.* TO 'ecomuser'@'localhost';
-MariaDB > FLUSH PRIVILEGES;
+CREATE DATABASE ecomdb;
+CREATE USER 'ecomuser'@'localhost' IDENTIFIED BY 'ecompassword';
+GRANT ALL PRIVILEGES ON *.* TO 'ecomuser'@'localhost';
+FLUSH PRIVILEGES;
